b4897bbb0a289c6d43a7a12529f48d384f414088
nNown <wojciechmar2000@gmail.com>
2021-01-17 23:14:48 +0100

    Started restriction map algorithm

--- src/instance_generator.cpp
+++ src/instance_generator.cpp
@@ -7,13 +7,16 @@ std::pair<std::vector<std::size_t>, std::vector<std::size_t>> InstanceGenerator:
     if(range.first >= range.second || cutsQuantity < 1) throw std::runtime_error("Incorrectly formated range or non-positive cuts quantity");
     
     std::vector<std::size_t> restrictionMap = std::vector<std::size_t>(cutsQuantity + 1);
-    for(auto& element : restrictionMap) {
-        element = 1 + std::rand() % range.second + 1;
+    for(auto& site : restrictionMap) {
+        std::size_t element = 0;
+        while(std::find(std::begin(restrictionMap), std::end(restrictionMap), element) != std::end(restrictionMap)) element = range.first + std::rand() % range.second + 1;
+
+        site = element;
     }
     
     std::vector<std::size_t> instance = std::vector<std::size_t>();
     std::size_t summedElementsNumber = 1;
-    while(summedElementsNumber <= cutsQuantity + 1) {
+    while(summedElementsNumber <= restrictionMap.size()) {
         for(std::size_t i = 0; i <= restrictionMap.size() - summedElementsNumber; i++) {
             std::size_t currentElement = 0;
             for(std::size_t j = i; j < i + summedElementsNumber; j++) {
@@ -29,11 +32,13 @@ std::pair<std::vector<std::size_t>, std::vector<std::size_t>> InstanceGenerator:
 }
 
 std::size_t InstanceGenerator::BinomialCoefficient(std::size_t n, std::size_t k) {
-    double result = 1;
-    for(double i = 1.0; i <= k; i++) {
-        result *= ((n + 1) - i)/i;
+    std::size_t counter = 1;
+    std::size_t denominator = 1;
+    for(std::size_t i = 1; i <= k; i++) {
+        counter *= (n + 1) - i;
+        denominator *= i;
     }
-    return std::floor(result);
+    return counter / denominator;
 }
 
 InstanceGenerator* InstanceGenerator::Generator() {
--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,95 @@
 #include <cstdlib>
 #include <time.h>
 
+bool findRestrictionMap(std::vector<std::size_t> restrictiveDigestionResult, std::vector<std::size_t>& map, std::size_t depth);
+
 int main(void) {
     std::srand(std::time(NULL));
     
     InstanceParser* instanceParser = InstanceParser::Parser();
     InstanceGenerator* instanceGenerator = InstanceGenerator::Generator();
 
-    auto instance = instanceGenerator->GenerateInstance({ 2, 8 }, 2, true);
-    instanceParser->SaveInstance(instance.first, "./tests");
-    instanceParser->SaveInstance(instance.second, "./results");
+    auto instance = instanceGenerator->GenerateInstance({ 2, 6 }, 2, true);
+    // instanceParser->SaveInstance(instance.first, "./tests");
+    // instanceParser->SaveInstance(instance.second, "./results");
+
+    
+    std::size_t n = 1, result = instanceGenerator->BinomialCoefficient(n + 2, 2);
+    while(result < instance.first.size()) {
+        n++;
+        result = instanceGenerator->BinomialCoefficient(n + 2, 2);
+    }
+
+    std::vector<std::size_t> answer = std::vector<std::size_t>(n);
+    std::cout << answer.size() << std::endl;
+    for(auto& element : instance.first) {
+        std::cout << element << " ";
+    }
     std::cout << std::endl;
+    
+    if(result == instance.first.size()) {
+        bool test = findRestrictionMap(instance.first, answer, 0);
+        std::cout << (test ? "True" : "False") << std::endl;
+        for(auto& element : answer) {
+            std::cout << element << " ";
+        }
+        std::cout << std::endl;
+    }
 
     return 0;
+}
+
+bool findRestrictionMap(std::vector<std::size_t> restrictiveDigestionResult, std::vector<std::size_t>& map, std::size_t depth) {
+    if(depth == map.size()) return true;
+
+    std::size_t sequenceLength = 0;
+    for(auto& fragment : restrictiveDigestionResult) {
+        if(fragment > sequenceLength) {
+            sequenceLength = fragment;
+        }
+    }
+    if(depth == 0) {
+        std::size_t secondLongestFragment = 0;
+        for(auto& fragment : restrictiveDigestionResult) {
+            if(fragment > secondLongestFragment && fragment < sequenceLength) {
+                secondLongestFragment = fragment;
+            }
+        }
+
+        std::size_t firstFragment = sequenceLength - secondLongestFragment;
+        auto firstFragmentPositionInResult = std::find(std::begin(restrictiveDigestionResult), std::end(restrictiveDigestionResult), firstFragment);
+        auto firstFragmentPositionBackwards = std::find(std::begin(restrictiveDigestionResult), std::end(restrictiveDigestionResult), sequenceLength - firstFragment);
+
+        if(firstFragmentPositionInResult != std::end(restrictiveDigestionResult) && firstFragmentPositionBackwards != std::end(restrictiveDigestionResult)) {
+            restrictiveDigestionResult.erase(firstFragmentPositionInResult);
+            restrictiveDigestionResult.erase(firstFragmentPositionBackwards);
+            map[depth] = firstFragment;
+            findRestrictionMap(restrictiveDigestionResult, map, depth + 1);
+        }
+
+        return false;
+    } else {
+        for(auto& fragmentLength : restrictiveDigestionResult) {
+            std::cout << "Depth: " << depth << " : " << fragmentLength << std::endl;
+            std::size_t summedMapFragments = 0;
+            for(auto& fragment : map) {
+                std::cout << fragment << " ";
+                summedMapFragments += fragment;
+            }
+            std::cout << std::endl;
+            
+            auto fragmentPositionInResult = std::find(std::begin(restrictiveDigestionResult), std::end(restrictiveDigestionResult), summedMapFragments);
+            auto fragmentPositionBackwards = std::find(std::begin(restrictiveDigestionResult), std::end(restrictiveDigestionResult), sequenceLength - summedMapFragments);
+            if(fragmentPositionInResult != std::end(restrictiveDigestionResult) && fragmentPositionBackwards != std::end(restrictiveDigestionResult)) {
+                restrictiveDigestionResult.erase(fragmentPositionInResult);
+                restrictiveDigestionResult.erase(fragmentPositionBackwards);
+                map[depth] = fragmentLength;
+                findRestrictionMap(restrictiveDigestionResult, map, depth + 1);
+            }
+
+            if(depth == map.size()) break;
+        }
+    }
+
+    return false;
 }
