e044ce82411f97a18d3ed416f1f8208b79df1539
nNown <wojciechmar2000@gmail.com>
2021-02-26 17:31:47 +0100

    README added

---
+++ README.md
@@ -0,0 +1,20 @@
+# Single digestion mapping
+Application takes path to series of fragments lenghts which are result of single restrictive digestion experiment. Based on those information user gets restriction map as series of fragments lengths in correct order (Can be read forwards as well as backwards). 
+
+## Parameters
+
+| Parameter                        | Data type |
+|----------------------------------|-----------|
+| Path to saved instance (path)    | String    | 
+
+### Installation
+
+```bash
+make
+```
+
+### Usage
+
+```bash
+./main path
+```
--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char* argv[]) {
     
     InstanceParser* instanceParser = InstanceParser::Parser();
     InstanceGenerator* instanceGenerator = InstanceGenerator::Generator();
-
+    
     auto instance = Instance(instanceParser->LoadInstance(argv[1]));
     if(instance.ExpectedMapFragments() > 0) {
         std::cout << "Restriction map: "; 
@@ -33,42 +33,6 @@ int main(int argc, char* argv[]) {
     } else {
         std::cout << "Instance doesn't contain any possible result" << std::endl;
     }
-
-    // bool instanceDirection[] = { false, true };
-    // int cutsQuantity[] = { 5, 8, 11, 14, 17, 20, 23 };
-    // std::pair<int, int> ranges[] = { { 33, 68 }, { 70, 125 }, { 140, 200 }, { 216, 245 } };
-
-    // for(auto& cutsNumber : cutsQuantity) {
-    //     for(auto& range : ranges) {
-    //         for(auto& direction : instanceDirection) {
-    //             auto generatedInstance = instanceGenerator->GenerateInstance(range, cutsNumber, direction);
-    //             auto instance = Instance(generatedInstance.first);
-    //             std::cout << "Parameters: " << std::endl;
-    //             std::cout << "\tCuts quantity: " << cutsNumber << std::endl;
-    //             std::cout << "\tFragment length range: " << range.first << " - " << range.second << std::endl;
-    //             std::cout << "\tDirection: " << (direction ? "Descending" : "Ascending") << std::endl;
-    //             std::cout << std::endl;
-
-    //             std::cout << "Generated instance: ";
-    //             for(auto& fragment : generatedInstance.first) {
-    //                 std::cout << fragment << " ";
-    //             }
-    //             std::cout << std::endl << "Expected result: ";
-    //             for(auto& fragment: generatedInstance.second) {
-    //                 std::cout << fragment << " ";
-    //             }
-    //             std::cout << std::endl << "Algorithm result: ";
-    //             auto start = std::chrono::high_resolution_clock::now();
-    //             findRestrictionMap(instance, 0);
-    //             auto stop = std::chrono::high_resolution_clock::now();
-    //             std::cout << std::endl;
-
-    //             auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-    //             std::cout << "Got in " << (duration.count() * 0.000001) << " seconds" << std::endl;
-    //             std::cout << std::endl;
-    //         }
-    //     }
-    // }
 
     return 0;
 }
